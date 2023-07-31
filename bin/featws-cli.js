7161a874c6a90d6779e9de6407369e3c0ea1bd62
Raphael Pinto <raphaelcpinto@gmail.com>
2023-07-31 16:00:42 -0300

    fix: relative dir

--- bin/featws-cli.js
+++ bin/featws-cli.js
@@ -1,7 +1,11 @@
 #!/usr/bin/env node
 
+const path = require("path");
+
 const { version } = require('../package.json');
+
 const { Command } = require('commander');
+
 const { check } = require('../lib/checker');
 const { transpiler } = require('../lib/transpiler');
 
@@ -15,18 +19,22 @@ program
 program.command('check')
     .description('Execute the checker')
     .option('-d, --directory <string>', 'rules directory', process.cwd())
-    .action(async (options) => check(options.directory));
+    .action(async (options) => {
+        const dir = path.isAbsolute(options.directory) ? options.directory : path.join(process.cwd(), options.directory)
+        check(dir)
+    });
 
 program.command('transpile')
     .description('Execute the transpiler')
     .option('--no-check', 'disable check')
     .option('-d, --directory <string>', 'rules directory', process.cwd())
     .action(async (options) => {
+        const dir = path.isAbsolute(options.directory) ? options.directory : path.join(process.cwd(), options.directory)
         try {
             if (options.check) {
-                await check(options.directory);
+                await check(dir);
             }
-            await transpiler(options.directory);
+            await transpiler(dir);
         } catch (e) {
             console.error(e);
         }
